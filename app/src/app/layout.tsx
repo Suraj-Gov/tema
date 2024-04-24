3de6d2fb0fbe6b8ff91fcf2de27918cc1766579c
Suraj Govind <surajgovind332000@gmail.com>
2024-04-24 07:37:24 +0530

    init: nextjs/tw/radix

---
+++ app/src/app/layout.tsx
@@ -0,0 +1,26 @@
+import { Theme } from "@radix-ui/themes";
+import "@radix-ui/themes/styles.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Tema",
+  description: "Build your dream theme",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Theme>{children}</Theme>
+      </body>
+    </html>
+  );
+}
